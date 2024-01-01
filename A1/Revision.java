2e8671ca218698fcdb4c43e697a4aa7d02f1f648
Shashi raj <shashiraj13457@gmail.com>
2024-01-01 19:16:30 +0530

    Revision

--- A1/Revision.java
+++ A1/Revision.java
@@ -1,8 +1,57 @@
 package A1;
-
+import java.util.Scanner;
 public class Revision {
-    //Largest number (input 3 no.)
+    // Ques1: Largest number (input 3 no.) method 1(self)
+
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        int c = input.nextInt();
+//
+//        if (a > b){
+//            if (a > c){
+//                System.out.println("greater number is: " + a);
+//            }
+//            else {
+//                System.out.println("greater number is: " + c);
+//            }
+//        } else {
+//            if (b > c){
+//                System.out.println("greater number is: " + b);
+//            }
+//            else {
+//                System.out.println("greater number is: " + c);
+//            }
+//        }
+//    }
+
+    // Method 2
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        int c = input.nextInt();
+//
+//        int max = a;
+//        if (b > max){
+//            max = b;
+//        }
+//        if (c > max){
+//            max = c;
+//        }
+//        System.out.println("Greatest number is "+max);
+
+        //Method 3
+//        int max = Math.max(c,Math.max(a,b));
+//        System.out.println(max);
+//    }
+
+
+    // Ques2: Check whether the letter you have input is in uppercase or lowercase(CaseCheck)
     public static void main(String[] args) {
         
     }
 }
+
+
